b2309ad99efbae1fd1f2abd7426d78bf40c94ac3
bhujel12 <71912832+bhujel12@users.noreply.github.com>
2022-06-10 14:24:11 -0400

    RocketThrustAndSoundAdded

--- Assets/Scripts/Motion.cs
+++ Assets/Scripts/Motion.cs
@@ -8,11 +8,13 @@ public class Motion : MonoBehaviour
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotationThrust = 100f;
     Rigidbody rb;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,6 +29,14 @@ public class Motion : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying) 
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
